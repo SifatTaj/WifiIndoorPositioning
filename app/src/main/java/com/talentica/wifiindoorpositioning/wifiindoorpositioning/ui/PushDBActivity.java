af2e874bf2a4303501cb32122927bca4ab411a37
TAJ <taj@TAJs-MacBook.local>
2019-10-30 03:30:51 +0600

    added DB name and progress bar

--- app/src/main/java/com/talentica/wifiindoorpositioning/wifiindoorpositioning/ui/PushDBActivity.java
+++ app/src/main/java/com/talentica/wifiindoorpositioning/wifiindoorpositioning/ui/PushDBActivity.java
@@ -1,61 +1,53 @@
 package com.talentica.wifiindoorpositioning.wifiindoorpositioning.ui;
 
-import android.print.PrintDocumentAdapter;
-import android.support.annotation.NonNull;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.Continuation;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
-import com.mongodb.client.model.InsertManyOptions;
 import com.mongodb.stitch.android.core.Stitch;
 import com.mongodb.stitch.android.core.StitchAppClient;
 import com.mongodb.stitch.android.core.auth.StitchUser;
 import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoClient;
 import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoCollection;
 import com.mongodb.stitch.core.auth.providers.anonymous.AnonymousCredential;
-import com.mongodb.stitch.core.services.mongodb.remote.RemoteInsertManyResult;
 import com.mongodb.stitch.core.services.mongodb.remote.RemoteInsertOneResult;
-import com.mongodb.stitch.core.services.mongodb.remote.RemoteUpdateOptions;
-import com.mongodb.stitch.core.services.mongodb.remote.RemoteUpdateResult;
 import com.talentica.wifiindoorpositioning.wifiindoorpositioning.R;
 import com.talentica.wifiindoorpositioning.wifiindoorpositioning.model.AccessPoint;
 import com.talentica.wifiindoorpositioning.wifiindoorpositioning.model.IndoorProject;
 import com.talentica.wifiindoorpositioning.wifiindoorpositioning.model.ReferencePoint;
 
-import org.bson.BsonDocument;
 import org.bson.Document;
-import org.bson.codecs.configuration.CodecRegistry;
-import org.bson.conversions.Bson;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import io.realm.Realm;
 import io.realm.RealmList;
+import io.realm.RealmObject;
 
 public class PushDBActivity extends AppCompatActivity {
 
     Button btnPush;
     TextView tvStatus;
-    EditText etCollection;
+    EditText etCollection, etDB;
+    ProgressBar progressBar;
 
     String projectId;
     Boolean dbReady = false;
+    double uploadProgress = 0;
+    Document doc;
 
     StitchAppClient client;
     RemoteMongoClient mongoClient;
 
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,11 +56,17 @@ public class PushDBActivity extends AppCompatActivity {
 
         btnPush = findViewById(R.id.btn_push);
         etCollection = findViewById(R.id.et_collection);
-        tvStatus = findViewById(R.id.tv_db_status);
+        etDB = findViewById(R.id.et_db);
+        tvStatus = findViewById(R.id.tv_upload_status);
+        progressBar = findViewById(R.id.pb_upload);
 
         projectId = getIntent().getStringExtra("projectId");
 
-        initDB();
+        IndoorProject project = Realm.getDefaultInstance().where(IndoorProject.class).equalTo("id", projectId).findFirst();
+        etDB.setText(project.getName());
+        etCollection.setText(project.getName());
+
+        initApi();
 
         btnPush.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,7 +76,7 @@ public class PushDBActivity extends AppCompatActivity {
         });
     }
 
-    protected void initDB() {
+    protected void initApi() {
         client = Stitch.initializeDefaultAppClient("rssi-lgpnr");
         mongoClient = client.getServiceClient(RemoteMongoClient.factory, "mongodb-atlas-rssi");
     }
@@ -91,30 +89,41 @@ public class PushDBActivity extends AppCompatActivity {
         final RealmList<AccessPoint> accessPointsFromRealm = project.getAps();
         final RealmList<ReferencePoint> referencePointsFromRealm = project.getRps();
 
-        final RemoteMongoCollection<Document> apCollection = mongoClient.getDatabase("bracu").getCollection(collection + "_ap");
-        final RemoteMongoCollection<Document> rpCollection = mongoClient.getDatabase("bracu").getCollection(collection + "_rp");
+        final String dbName = etDB.getText().toString();
+
+        final RemoteMongoCollection<Document> apCollection = mongoClient.getDatabase(dbName).getCollection(collection + "_ap");
+        final RemoteMongoCollection<Document> rpCollection = mongoClient.getDatabase(dbName).getCollection(collection + "_rp");
         client.getAuth().loginWithCredential(new AnonymousCredential()).addOnSuccessListener(new OnSuccessListener<StitchUser>() {
             @Override
             public void onSuccess(StitchUser stitchUser) {
-                Toast.makeText(getApplicationContext(), "Login", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Authorised Access", Toast.LENGTH_SHORT).show();
             }
         });
 
+        double size = accessPointsFromRealm.size() + referencePointsFromRealm.size();
+        double incr = 100.0 / size;
+
         for (AccessPoint accessPoint : accessPointsFromRealm) {
-            Document doc = new Document();
+            doc = new Document();
             doc.put("ssid", accessPoint.getSsid());
             doc.put("mac",accessPoint.getMac_address());
             doc.put("description", accessPoint.getDescription());
             doc.put("x", accessPoint.getX());
             doc.put("y", accessPoint.getY());
-            apCollection.insertOne(doc);
+            apCollection.insertOne(doc).addOnSuccessListener(new OnSuccessListener<RemoteInsertOneResult>() {
+                @Override
+                public void onSuccess(RemoteInsertOneResult remoteInsertOneResult) {
+                    progressBar.setProgress((int) uploadProgress);
+                    tvStatus.setText("Uploaded");
+                }
+            });
+            uploadProgress += incr;
         }
 
         for (ReferencePoint referencePoint : referencePointsFromRealm) {
 
             List<Double> readings = new ArrayList<>();
             for (AccessPoint accessPoint : referencePoint.getReadings()) {
-                DBObject dbObject = new BasicDBObject(accessPoint.getMac_address(), accessPoint.getMeanRss());
                 readings.add(accessPoint.getMeanRss());
             }
 
@@ -124,7 +133,13 @@ public class PushDBActivity extends AppCompatActivity {
             doc.put("x", referencePoint.getX());
             doc.put("y", referencePoint.getY());
             doc.put("readings", readings);
-            rpCollection.insertOne(doc);
+            rpCollection.insertOne(doc).addOnSuccessListener(new OnSuccessListener<RemoteInsertOneResult>() {
+                @Override
+                public void onSuccess(RemoteInsertOneResult remoteInsertOneResult) {
+                    progressBar.setProgress((int) uploadProgress);
+                }
+            });
+            uploadProgress += incr;
         }
     }
 }
